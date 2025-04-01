42d7fc03fa7547b95a7d0723a1e57beb97490951
1DanDev <4dandev@gmail.com>
2025-03-31 19:15:25 -0600

    Is user active added

--- src/test/java/com/eddy/chatapp/core/ContactosTest.java
+++ src/test/java/com/eddy/chatapp/core/ContactosTest.java
@@ -99,4 +99,25 @@ public class ContactosTest {
         contactos.deleteContact(user1.getId());
         contactos.deleteContact(user2.getId());
     }
+
+    @Test
+    void testIsUserActive() {
+        Users user1 = new Users("AA:BB:CC:DD:EE:FF", "Juan", new byte[]{1, 2, 3});
+        Users user2 = new Users("11:22:33:44:55:66", "Maria", new byte[]{4, 5, 6});
+
+        contactos.addContact(user1);
+        contactos.addContact(user2);
+
+        List<Users> activeUsers = contactos.isUserActive();
+        assertNotNull(activeUsers);
+        assertEquals(2, activeUsers.size());
+
+        // Verificar que los usuarios activos sean los esperados
+        assertTrue(activeUsers.stream().anyMatch(user -> "AA:BB:CC:DD:EE:FF".equals(user.getId())));
+        assertTrue(activeUsers.stream().anyMatch(user -> "11:22:33:44:55:66".equals(user.getId())));
+
+        // Eliminar los contactos después del test
+        contactos.deleteContact(user1.getId());
+        contactos.deleteContact(user2.getId());
+    }
 }
